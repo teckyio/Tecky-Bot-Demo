752f6927ca76aab5b939b21df9a36fd72ae431fb
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-11-16 15:02:18 +0800

    Added basic bot responses.

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,16 @@
 
 
 function botAnswers(sentences){
-    return "hello";
+    const words = window.nlp(sentences);
+    console.log(words.people().data().length);
+    if(words.match('hello').found|| words.match('hi').found){
+        return "Hello! How do I call you Sir/Madem?";
+    }else if(words.people().data().length > 0){
+        const firstPersonName = words.people().data()[0].text;
+        return `Hi ${firstPersonName}! Nice to meet you!`;
+    }else if(words.match('age').found || words.match('How old').found){
+        return "I am just several days old."
+    }else{
+        return `Do you mean ${sentences}? Am I understanding it correctly?`;
+    }
 }
