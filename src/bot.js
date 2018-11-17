48e77585205b9a2340a61e565aab677b072d8f1e
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-11-17 12:58:24 +0800

    Fixed the people API.

--- src/bot.js
+++ src/bot.js
@@ -6,7 +6,7 @@ function botAnswers(sentences){
     if(words.match('hello').found|| words.match('hi').found){
         return "Hello! How do I call you Sir/Madam?";
     }else if(words.people().data().length > 0){
-        const firstPersonName = words.people().data()[0].text;
+        const firstPersonName = words.people(0).text();
         return `Hi ${firstPersonName}! Nice to meet you!`;
     }else if(words.match('age').found || words.match('How old').found){
         return "I am just several days old."
